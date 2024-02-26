ca694b7da48d42c61fb133a0f8afb4267b16b22c
Ehsan Mohammadi <ehsan@selected.org>
2024-02-26 09:50:49 +0330

    refactor : Add setQueriesData

--- src/features/home/hooks/useDeleteBook.ts
+++ src/features/home/hooks/useDeleteBook.ts
@@ -1,5 +1,7 @@
 import {useMutation, UseMutationResult} from '@tanstack/react-query';
 import {BookID, deleteBook} from 'features/home/api/home.ts';
+import queryClient from 'core/config/reactQuery.ts';
+import {Book} from 'features/home/type/book.ts';
 
 export function useDeleteBook(): UseMutationResult<
   any,
@@ -10,5 +12,12 @@ export function useDeleteBook(): UseMutationResult<
   return useMutation({
     mutationKey: ['deleteBook'],
     mutationFn: (body: BookID) => deleteBook(body),
+    onSuccess: (res, body) => {
+      queryClient.setQueriesData(
+        {queryKey: ['book']},
+        (oldData: Book[] | undefined): Book[] | undefined =>
+          oldData?.filter(book => book.id != body.id),
+      );
+    },
   });
 }
