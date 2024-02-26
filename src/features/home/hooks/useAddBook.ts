ee5701c7169c484c704af07095ffc7ab1dbbece3
Ehsan Mohammadi <ehsan@selected.org>
2024-02-26 17:26:07 +0330

    refactor : Add refetchQueries

--- src/features/home/hooks/useAddBook.ts
+++ src/features/home/hooks/useAddBook.ts
@@ -1,9 +1,15 @@
 import {useMutation, UseMutationResult} from '@tanstack/react-query';
 import {AddBook, addBook} from 'features/home/api/home.ts';
+import {AxiosError} from 'axios';
+import queryClient from 'core/config/reactQuery.ts';
 
 export function useAddBook(): UseMutationResult<any, Error, AddBook, unknown> {
   return useMutation({
     mutationKey: ['addBook'],
     mutationFn: (body: AddBook) => addBook(body),
+    onSuccess: async () => {
+      await queryClient.refetchQueries({queryKey: ['book']});
+    },
+    onError: (e: AxiosError) => {},
   });
 }
--- src/features/home/hooks/useUpdateBook.ts
+++ src/features/home/hooks/useUpdateBook.ts
@@ -1,5 +1,7 @@
 import {useMutation, UseMutationResult} from '@tanstack/react-query';
 import {UpdateBook, updateBook} from 'features/home/api/home.ts';
+import {AxiosError} from 'axios';
+import queryClient from 'core/config/reactQuery.ts';
 
 export function useUpdateBook(): UseMutationResult<
   any,
@@ -10,5 +12,9 @@ export function useUpdateBook(): UseMutationResult<
   return useMutation({
     mutationKey: ['updateBook'],
     mutationFn: (body: UpdateBook) => updateBook(body),
+    onSuccess: async () => {
+      await queryClient.refetchQueries({queryKey: ['book']});
+    },
+    onError: (e: AxiosError) => {},
   });
 }
