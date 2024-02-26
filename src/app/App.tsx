8f78bdc16fc9bccefc9f70f4e573499f40616f3f
Ehsan Mohammadi <ehsan@selected.org>
2024-02-26 09:51:48 +0330

    refactor : Add StatusBar and NavigationContainer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,25 +6,33 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import AppNavigation from 'app/navigation/AppNavigation';
 import queryClient from 'core/config/reactQuery.ts';
 import {QueryClientProvider} from '@tanstack/react-query';
+import {NavigationContainer} from '@react-navigation/native';
+import palette from 'core/styles/palette.ts';
 
 const App = (): React.JSX.Element => {
   return (
     <SafeAreaView style={styles.container}>
-      <QueryClientProvider client={queryClient}>
-        <AppNavigation />
-      </QueryClientProvider>
+      <NavigationContainer>
+        <QueryClientProvider client={queryClient}>
+          <StatusBar
+            animated={true}
+            barStyle={'dark-content'}
+            backgroundColor={palette.backgroundColor}
+          />
+          <AppNavigation />
+        </QueryClientProvider>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    flex: 1,
   },
 });
 
