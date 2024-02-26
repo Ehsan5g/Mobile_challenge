dca6186169eadaf19ec7a9d246d1b283fca34d4d
Ehsan Mohammadi <ehsan@selected.org>
2024-02-26 17:26:45 +0330

    refactor : Add RefreshControl

--- src/features/home/screens/Home.tsx
+++ src/features/home/screens/Home.tsx
@@ -1,7 +1,13 @@
 import React, {useCallback, useMemo, useState} from 'react';
 
-import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {
+  ActivityIndicator,
+  RefreshControl,
+  StyleSheet,
+  View,
+} from 'react-native';
 import palette from 'core/styles/palette.ts';
+import pallet from 'core/styles/palette.ts';
 import {useGetBook} from 'features/home/hooks/useGetBook.ts';
 import Text from 'core/components/Text';
 import BookList from 'features/home/components/BookList';
@@ -10,13 +16,27 @@ import Input from 'core/components/Input';
 import {Book} from 'features/home/type/book.ts';
 import MessageModal from 'core/components/MessageModal';
 import {useDeleteBook} from 'features/home/hooks/useDeleteBook.ts';
+import Button from 'core/components/Button';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  AppNavigatorParam,
+  TypeCreateBook,
+} from 'app/navigation/AppNavigation.tsx';
+import {AppRoutes} from 'app/navigation/routes.ts';
 
 const Home = () => {
-  const [message, setMessage] = useState<Book | undefined>(undefined);
-  const {data, isPending: getLoading} = useGetBook({id: undefined});
+  const {navigate} =
+    useNavigation<NavigationProp<AppNavigatorParam, AppRoutes.Home>>();
 
+  const [message, setMessage] = useState<Book | undefined>(undefined);
   const [search, setSearch] = useState('');
 
+  const {
+    data,
+    isPending: getLoading,
+    refetch,
+    isRefetching,
+  } = useGetBook({id: undefined});
   const {mutateAsync: deleteBook, isPending: deleteLoading} = useDeleteBook();
 
   const filteredData = useMemo(() => {
@@ -24,8 +44,8 @@ const Home = () => {
       ? data?.filter(
           item =>
             item.title.toLowerCase().includes(search.toLowerCase()) ||
-            item.author.toLowerCase().includes(search.toString()) ||
-            item.genre.toLowerCase().includes(search.toString()) ||
+            item.author.toLowerCase().includes(search.toLowerCase()) ||
+            item.genre.toLowerCase().includes(search.toLowerCase()) ||
             item.yearPublished.toString().includes(search),
         )
       : data;
@@ -45,17 +65,47 @@ const Home = () => {
   }
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Icon name={'magnify'} size={24} color={palette.border} />
-        <Input
-          style={styles.input}
-          placeholder={'Search'}
-          value={search}
-          onChangeText={setSearch}
-        />
+      <View>
+        <View style={styles.header}>
+          <Text size={'xxl'}>Books</Text>
+          <Button
+            style={styles.button}
+            onPress={() => {
+              navigate(AppRoutes.CREATE_BOOK, {
+                type: TypeCreateBook.ADD,
+                book: undefined,
+              });
+            }}
+            title={'Add new Book'}
+            iconRight={'plus'}
+            color={'white'}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name={'magnify'} size={24} color={palette.border} />
+          <Input
+            style={styles.input}
+            placeholder={'Search'}
+            value={search}
+            onChangeText={setSearch}
+          />
+        </View>
       </View>
       <BookList
         onDelete={onDelete}
+        refreshControl={
+          <RefreshControl
+            colors={[palette.primary, pallet.secondary]}
+            refreshing={isRefetching}
+            onRefresh={() => refetch()}
+          />
+        }
+        onEdit={book => {
+          navigate(AppRoutes.CREATE_BOOK, {
+            type: TypeCreateBook.EDITE,
+            book: book,
+          });
+        }}
         data={filteredData}
         showsVerticalScrollIndicator={false}
       />
@@ -64,8 +114,8 @@ const Home = () => {
         onCancel={() => setMessage(undefined)}
         onConfirm={async () => {
           if (message != undefined) {
-            await deleteBook({id: message.id});
             setMessage(undefined);
+            await deleteBook({id: message.id});
           }
         }}
         title={'Delete the book'}
@@ -80,6 +130,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: palette.backgroundColor,
     paddingTop: 12,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    justifyContent: 'space-between',
   },
   loadingContainer: {
     flex: 1,
@@ -101,6 +157,9 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     borderWidth: 0,
+  },
+  button: {
+    paddingHorizontal: 12,
   },
 });
 
